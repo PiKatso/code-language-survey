8380249638bd158157d39c4eefa0c892bbab1488
Kat Gifford <katgiff2007@gmail.com>
2017-03-24 13:13:33 -0700

    add logic, correct functions and css

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// $(document).ready(function() {
+//   $("form#submit").submit(function(event)
+$(document).ready(function(){
+ $("form#question").submit(function(event) {
+
+   var question1 = $("#who").val();
+   var question2 = $("#where").val();
+   var question3 = $("#what").val();
+   var question4 = $("#how").val();
+   var question5 = $("#when").val();
+
+
+   if (question5 === "Obsolete"){
+      $("#rethink").show();
+    } else if (question1 === "Be a professional developer"){
+      $("#ruby").show();
+    } else if (question2 === "Established employer"){
+      $("#c-sharp").show();
+    } else if (question3 === "Branding Websites"){
+      $("#php").show();
+    } else (question4 === "How things work (logic)"){
+      $("#c-sharp").show();
+    }
+      
+    event.preventDefault();
+  });
+});
