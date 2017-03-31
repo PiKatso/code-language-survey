a67e97ca5361b13c2fd585f4cb91ed0a09c016fc
Kat Gifford <katgiff2007@gmail.com>
2017-03-30 20:14:54 -0700

    correct last spelling error in code

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
       $("#c-sharp").hide();
       $("#ruby").hide();
       $("#php").hide();
-    } else if ((question1 === "To run a startup<") || (question2 === "Start ups")){
+    } else if ((question1 === "To run a startup") || (question2 === "Start ups")){
       $("#rethink").hide();
       $("#c-sharp").hide();
       $("#ruby").show();
