af4c75f2a074f09f8a53f3c934078fd481bc880c
Kat Gifford <katgiff2007@gmail.com>
2017-03-30 20:29:16 -0700

    update jQuery logic to not have redundancies

--- js/scripts.js
+++ js/scripts.js
@@ -13,36 +13,21 @@ $(document).ready(function(){
       $("#c-sharp").hide();
       $("#ruby").hide();
       $("#php").hide();
-    } else if ((question1 === "To run a startup") || (question2 === "Start ups")){
+    } else if ((question1 === "To run a startup") || (question2 === "Start ups")|| (question4 === "How to interact with things (UX)")){
       $("#rethink").hide();
       $("#c-sharp").hide();
       $("#ruby").show();
       $("#php").hide();
-    } else if ((question2 === "Established employer") || (question3 === "Software Development")){
+    } else if ((question2 === "Established employer") || (question4 === "How things work (logic)") || (question3 === "Software Development")){
       $("#c-sharp").show();
       $("#rethink").hide();
       $("#ruby").hide();
       $("#php").hide();
-    } else if ((question3 === "Mobile Aps") || (question2 === "Enterprise")){
+    } else if ((question3 === "Mobile Aps") || (question2 === "Enterprise") || (question1 === "To build something fun/useful")){
       $("#rethink").hide();
       $("#c-sharp").hide();
       $("#ruby").hide();
       $("#php").show();
-    } else if ((question4 === "How things work (logic)") || (question1 === "Established employer")){
-      $("#c-sharp").show();
-      $("#rethink").hide();
-      $("#php").hide();
-      $("#ruby").hide();
-    } else if (question1 === "Be a professional developer") {
-      $("#rethink").hide();
-      $("#c-sharp").hide();
-      $("#ruby").show();
-      $("#php").hide();
-    } else if (question2 === "Established employer"){
-      $("#c-sharp").show();
-      $("#rethink").hide();
-      $("#ruby").hide();
-      $("#php").hide();
     } else {
        $("#rethink").show();
        $("#c-sharp").hide();
