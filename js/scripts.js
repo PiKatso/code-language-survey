6bd6dd719b9e8c7129fe10c9300ffb06af344e4e
Kat Gifford <katgiff2007@gmail.com>
2017-03-30 19:59:50 -0700

    add logic to hide unwanted results

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-// $(document).ready(function() {
-//   $("form#submit").submit(function(event)
 $(document).ready(function(){
  $("form#question").submit(function(event) {
 
@@ -12,20 +10,44 @@ $(document).ready(function(){
 
    if (question5 === "Obsolete"){
       $("#rethink").show();
+      $("#c-sharp").hide();
+      $("#php").hide();
+      $("#ruby").hide();
     } else if ((question1 === "To run a startup<") || (question2 === "Start ups")){
       $("#ruby").show();
+      $("#c-sharp").hide();
+      $("#php").hide();
+      $("#rethink").hide();
     } else if ((question2 === "Established employer") || (question3 === "Software Development")){
       $("#c-sharp").show();
+      $("#php").hide();
+      $("#ruby").hide();
+      $("#rethink").hide();
     } else if ((question3 === "Mobile Aps") || (question1 === "Enterprise")){
       $("#php").show();
+      $("#c-sharp").hide();
+      $("#ruby").hide();
+      $("#rethink").hide();
     } else if ((question4 === "How things work (logic)") || (question1 === "Established employer")){
       $("#c-sharp").show();
+      $("#php").hide();
+      $("#ruby").hide();
+      $("#rethink").hide();
     } else if (question1 === "Be a professional developer") {
       $("#ruby").show();
+      $("#c-sharp").hide();
+      $("#php").hide();
+      $("#rethink").hide();
     } else if (question2 === "Established employer"){
       $("#c-sharp").show();
+      $("#php").hide();
+      $("#ruby").hide();
+      $("#rethink").hide();
     } else {
        $("#rethink").show();
+       $("#c-sharp").hide();
+       $("#php").hide();
+       $("#ruby").hide();
     }
 
     event.preventDefault();
